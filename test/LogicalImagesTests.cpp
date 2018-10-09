f0fdf1ddedbdace6ca41cdaf75d59e008fc2305c
just44play <justyna.walkowiak6969@gmail.com>
2018-10-09 23:22:46 +0200

    Import JSON parsing library

--- test/LogicalImagesTests.cpp
+++ test/LogicalImagesTests.cpp
@@ -1,11 +1,17 @@
 #include <gtest/gtest.h>
+#include <json.hpp>
 #include "LogicalImages.hpp"
 
 struct LogicalImagesTests : public ::testing::Test
 {
 };
 
-TEST_F(LogicalImagesTests, assertThatTrueIsTrue)
+TEST_F(LogicalImagesTests, assertThatJSONParsingLibraryWorks)
 {
-    ASSERT_TRUE(true);
+    using json = nlohmann::json;
+
+    json j;
+    j["pi"] = 3.14;
+
+    ASSERT_TRUE(j["pi"] == 3.14);
 }
