14a75269e2dcafef335c9380039d6a9189d6569e
just44play <justyna.walkowiak6969@gmail.com>
2018-10-15 14:34:05 +0200

    Reading JSON from readme

--- test/BoardTests.cpp
+++ test/BoardTests.cpp
@@ -1,18 +1,29 @@
 #include "Board.hpp"
 #include "json.hpp"
 #include <gtest/gtest.h>
+#include <fstream>
 
 struct BoardTests : public ::testing::Test
 {
+    using json = nlohmann::json;
 };
 
 TEST_F(BoardTests, checkConstructorNoexception)
 {
-    nlohmann::json j;
+    json j;
     j["col"] = {{1, 1}, {1}};
     j["row"] = {{1}, {1}, {1}};
     j["rowNum"] = 3;
     j["colNum"] = 2;
 
     Board board(j["rowNum"], j["colNum"], j["row"], j["col"]);
+}
+
+TEST_F(BoardTests, checkReadingJSON)
+{
+    std::ifstream i("../test/ReadMeBoardTestFile.json");
+    json j;
+    i >> j;
+
+    Board board(j["rowNumber"], j["colNumber"], j["rows"], j["cols"]);
 }
