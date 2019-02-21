63d368d17eb784857a6a1510720590a047627561
majkel <michal.sobotka.84@gmail.com>
2019-02-21 10:33:04 +0100

    Drawing board - in progress

--- src/Board.cpp
+++ src/Board.cpp
@@ -1,5 +1,5 @@
 #include "Board.hpp"
-#include "json.hpp"
+//#include "json.hpp"
 //#include "JSONFileReader.hpp"
 #include <fstream>
 
@@ -27,4 +27,49 @@ int Board::getRowsNumber() const
 int Board::getColsNumber() const
 {
     return colNumber_;
+}
+
+std::string Board::drawPadding(const int & el) const
+{
+    std::string result;
+    for (auto it = 0; it < el; it++)
+        result += PADDING;
+    return result;
+}
+
+std::string Board::drawEndLine(const int & el) const
+{
+    std::string result {INTERSECTION};
+    for (auto it = 0; it < el; it++)
+        result += HORIZONTAL;
+    result += INTERSECTION;
+    return result;
+}
+
+std::string Board::drawEmptyLine(const int & el) const
+{
+    std::string result {VERTICAL};
+    for (auto it = 0; it < el; it++)
+        result += PADDING;
+    result += VERTICAL;
+    return result;
+}
+
+std::string Board::drawBoard(const int & rowNo, const int & colNo) const
+{
+    std::string result;
+    result += drawEndLine(colNo);
+    result += "\n";
+    for (auto it = 0; it < rowNo; it++)
+    {
+        result += drawEmptyLine(colNo);
+        result += "\n";
+    }
+    drawEndLine(colNo);
+    return result;
+}
+
+std::ostream & Board::operator<<( std::ostream & stream)
+{
+    return stream << drawBoard(rowNumber_, colNumber_);
 }
