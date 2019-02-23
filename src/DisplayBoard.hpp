1ed76963ebab6701c18d54e2df56a1d34087df54
PrzemekB <przemb@yahoo.com>
2019-02-23 18:52:05 +0100

    Helper functions from DisplayBoard encapsulated

--- src/DisplayBoard.hpp
+++ src/DisplayBoard.hpp
@@ -1,21 +1,14 @@
 #pragma once
 #include <sstream>
 
-const char HORIZONTAL = '-';
-const char VERTICAL = '|';
-const char INTERSECTION = '+';
-const char PADDING = ' ';
-
-std::string drawPadding(const int width);
-std::string drawEndLine(const int width);
-std::string drawEmptyLine(const int width);
-std::string drawColumns(const int maxElementsInRows, const int colNumber, const int heightCol);
-std::string drawRow(const int maxElementsInRows);
-std::stringstream drawBoard(const int width,
+namespace DisplayBoard
+{
+    std::stringstream drawBoard(const int width,
                             const int height,
                             const int widthRows,
                             const int heightCol);
 
+}
 
 
 
