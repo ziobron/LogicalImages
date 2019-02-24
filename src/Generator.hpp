94a7ae67caa7145be5f86f4e5ab8ea8e2757ba05
Jakub Zych <jzych89@gmail.com>
2019-02-24 11:26:40 +0100

    Generator: Add describtions to new functions

--- src/Generator.hpp
+++ src/Generator.hpp
@@ -1,5 +1,17 @@
 #pragma once
 #include "Board.hpp"
 
+// getAllVectorPermutation_ is a temporary function to check algorithm if it generates valid
+// and correct permutation for given vector when it can only store 2 values {0, 1}
+// input:
+//    int vSize - size of a vector to find all premutations
+// output:
+//    Lines result - vector of all posibble combinations of vector of given size
 Lines getAllVectorPermutation_(int vSize);
+
+// Fuction converts given board of values from range {0, 1} to corensponding valies of FieldsEnum
+// input:
+//     Lines board - board of int values to be converted
+// output:
+//     Blines result - board of FieldsEnum values
 BLines ConvertIntToFieldEnum(const Lines & board);
