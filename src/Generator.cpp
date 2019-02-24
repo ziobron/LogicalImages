19221aa24d6e2461cc2c54d56063fd7fef12a09f
Jakub Zych <jzych89@gmail.com>
2019-02-24 13:20:57 +0100

    Generator: Find all permuation for board of given size

--- src/Generator.cpp
+++ src/Generator.cpp
@@ -22,6 +22,21 @@ Lines getAllVectorPermutation_(int vSize)
     }
 }
 
+bool nextPermutation(Line &row)
+{
+    for (int i = row.size() - 1; ; --i)
+    {
+        if (i < 0)
+            return true;
+
+        if (++row[i] == 2)
+            row[i] = 0;
+        else
+            break;
+    }
+    return false;
+}
+
 BLines ConvertIntToFieldEnum(const Lines & board)
 {
     BLines result;
@@ -33,6 +48,27 @@ BLines ConvertIntToFieldEnum(const Lines & board)
             [](int elem){return (elem ? FieldsEnum::BLACK : FieldsEnum::WHITE);});
 
         result.emplace_back(row);
+    }
+    return result;
+}
+
+std::vector<Lines> GenerateAllBoardPermutations(unsigned int rowSize, unsigned int colSize)
+{
+    std::vector<Lines> result;
+    Lines board(rowSize, Line(colSize));
+    int i = 0;
+
+    while(true) {
+        result.emplace_back(board);
+
+        while (nextPermutation(board[i]))
+            if (++i >= rowSize)
+                break;
+
+        if (i >= rowSize)
+            break;
+        else
+            i = 0;
     }
     return result;
 }
--- test/GeneratorTests.cpp
+++ test/GeneratorTests.cpp
@@ -23,3 +23,21 @@ TEST_F(GeneratorTests, checkConversionFromIntToFieldsEnum)
 
     ASSERT_EQ(result, ConvertIntToFieldEnum(input));
 }
+
+TEST_F(GeneratorTests, checkSizeOfSAllPermutationOfBoard2x3)
+{
+    int rows = 2;
+    int cols = 3;
+    auto result = GenerateAllBoardPermutations(rows,cols);
+    std::set<Lines> s(result.begin(), result.end());
+    ASSERT_EQ(s.size(), pow(2,(rows * cols)));
+}
+
+TEST_F(GeneratorTests, checkSizeOfSAllPermutationOfBoard4x3)
+{
+    int rows = 4;
+    int cols = 3;
+    auto result = GenerateAllBoardPermutations(rows,cols);
+    std::set<Lines> s(result.begin(), result.end());
+    ASSERT_EQ(s.size(), pow(2,(rows * cols)));
+}
