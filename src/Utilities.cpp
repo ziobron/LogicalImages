97ce8fabf1875e63af0b3738895795898088f352
Adriana <agrzesiuk.7@gmail.com>
2019-02-25 18:16:55 +0100

    Utilities: Add tests and body to openFileToWrite function

--- src/Utilities.cpp
+++ src/Utilities.cpp
@@ -18,4 +18,17 @@ std::ifstream openFileToRead(const std::string & filename)
     }
 }
 
-
+std::ofstream openFileToWrite(const std::string & filename)
+{
+    try
+    {
+        std::ofstream data(filename);
+        data.exceptions(std::ofstream::failbit);
+        return data;
+    }
+    catch (std::exception)
+    {
+        std::cout << "Input operation failed (formatting or extraction error)." << std::endl;
+        throw;
+    }
+}
