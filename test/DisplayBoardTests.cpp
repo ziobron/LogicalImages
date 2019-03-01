c1849d1df05f3a4246a5b22527ff7002380a64d5
Jakub Zych <jzych89@gmail.com>
2019-03-01 12:15:47 +0100

    DisplayBoardTests: Remove private functions tests

--- test/DisplayBoardTests.cpp
+++ test/DisplayBoardTests.cpp
@@ -1,61 +1,11 @@
-//added for testing private methods
-//acccording to google test guide
-#include "DisplayBoard.cpp"
 #include "json.hpp"
 #include <gtest/gtest.h>
+#include "DisplayBoard.hpp"
 
 
 struct DisplayBoardTests : public ::testing::Test
 {
 };
-
-TEST_F(DisplayBoardTests, checkDrawPaddingFor2Elements)
-{
-    std::string comparePad = "    ";
-    std::string padding = drawPadding(2);
-
-    ASSERT_EQ(padding, comparePad);
-}
-
-TEST_F(DisplayBoardTests, checkDrawEndLineFor2Element)
-{
-    std::string comparePad = "+----+";
-    std::string endLine = drawEndLine(2);
-
-    ASSERT_EQ(endLine, comparePad);
-}
-
-TEST_F(DisplayBoardTests, checkDrawEmptyLineFor2Element)
-{
-    std::string comparePad = "|    |";
-    std::string emptyLine = drawEmptyLine(2);
-
-    ASSERT_EQ(emptyLine, comparePad);
-}
-
-TEST_F(DisplayBoardTests, checkDrawingEmptyColumnsForHeightOne)
-{
-    std::string compareColumns = "     +----+\n     |    |\n";
-    std::string drawCol = drawColumns(2, 2, 1);
-
-    ASSERT_EQ(compareColumns, drawCol);
-}
-
-TEST_F(DisplayBoardTests, checkDrawingEmptyColumnsForHeightTwo)
-{
-    std::string compareColumns = "     +----+\n     |    |\n     |    |\n";
-    std::string drawCol = drawColumns(2, 2, 2);
-
-    ASSERT_EQ(compareColumns, drawCol);
-}
-
-TEST_F(DisplayBoardTests, checkDrawingEmptyRowsForMaxOneElementInRow)
-{
-    std::string compareRow = "|  ";
-    std::string drawR = drawRow(1);
-
-    ASSERT_EQ(drawR, compareRow);
-}
 
 TEST_F(DisplayBoardTests, checkDrawingEmptyTable3x3)
 {
