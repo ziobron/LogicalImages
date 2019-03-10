405cda243d7aa8bfbc90cc52d5e08247ac8469f0
Martyna Łęcka <martynalecka.ml@gmail.com>
2019-03-10 18:30:19 +0100

    Changed test name and added another assertion.

--- test/DisplayBoardTests.cpp
+++ test/DisplayBoardTests.cpp
@@ -36,7 +36,7 @@ TEST_F(DisplayBoardTests, checkDisplayBoard)
     ASSERT_EQ(output, compareBoard);
 }
 
-TEST_F(DisplayBoardTests, checkDrawingTableWithCluesForColumns)
+TEST_F(DisplayBoardTests, checkDisplayBoardWithClues)
 {
     Board board(2, 2, {{1},{2}}, {{2},{1}});
     std::string compareBoard = "   +----+\n   | 2 1|\n+--+----+\n| 1|????|\n| 2|????|\n+--+----+\n";
@@ -54,4 +54,8 @@ TEST_F(DisplayBoardTests, checkDrawingTableWithCluesForColumns)
     Board board4(3, 3, {{1},{2,1},{}}, {{2,1},{1},{}});
     compareBoard = "     +------+\n     | 1    |\n     | 2 1  |\n+----+------+\n|   1|??????|\n| 2 1|??????|\n|    |??????|\n+----+------+\n";
     ASSERT_EQ(display(board4), compareBoard);
+
+    Board board5(3, 3, {{1},{1},{1,1}}, {{1},{},{1,1,1}});
+    compareBoard = "     +------+\n     |     1|\n     |     1|\n     | 1   1|\n+----+------+\n|   1|??????|\n|   1|??????|\n| 1 1|??????|\n+----+------+\n";
+    ASSERT_EQ(display(board5), compareBoard);
 }
