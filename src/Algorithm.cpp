885489325ee670f970a4c3bae88ebfec1185129a
Jakub Zych <jzych89@gmail.com>
2019-03-12 00:36:11 +0100

    Algorithm: Remove example algorithm

--- src/Algorithm.cpp
+++ src/Algorithm.cpp
@@ -2,10 +2,3 @@
 
 Algorithm::Algorithm() {}
 Algorithm::~Algorithm() {}
-
-AlgorithmA::AlgorithmA() {}
-bool AlgorithmA::solve(std::shared_ptr<Board> b)
-{
-    b->display();
-    return true;
-}
--- src/Algorithm.hpp
+++ src/Algorithm.hpp
@@ -3,19 +3,9 @@
 
 class Algorithm
 {
-// protected:
-//     Board board_;
 public:
     Algorithm();
     virtual ~Algorithm();
 
     virtual bool solve(std::shared_ptr<Board> b) = 0;
-};
-
-class AlgorithmA : public Algorithm
-{
-public:
-    AlgorithmA();
-
-    bool solve(std::shared_ptr<Board> b);
 };
