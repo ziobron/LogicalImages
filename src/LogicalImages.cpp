a571a24b9a96e6297c6598b9e0d05cf8521b82bf
Jakub Zych <jzych89@gmail.com>
2019-03-12 00:12:20 +0100

    LogicalImages: Add second constructor with file path

--- src/LogicalImages.cpp
+++ src/LogicalImages.cpp
@@ -10,6 +10,24 @@ LogicalImages::LogicalImages(std::shared_ptr<Board> b,
       algorithm_(a)
 {}
 
+LogicalImages::LogicalImages(std::string const path,
+                             std::shared_ptr<Algorithm> a)
+    : algorithm_(a)
+{
+    try
+    {
+        JSONFileReader jfr(path);
+        board_ = std::make_shared<Board>(jfr.readRowSize(),
+                                         jfr.readColSize(),
+                                         jfr.readCluesRows(),
+                                         jfr.readCluesCols());
+    }
+    catch(InvalidDimensions const& ex)
+    {
+        std::cout << "Invalid JSON file: " << ex.what() << std::endl;
+    }
+}
+
 LogicalImages::~LogicalImages() {}
 
 void LogicalImages::solve()
