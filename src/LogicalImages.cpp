c79143719d431c46ddd28b13d15bdc32cc2eda74
Jakub Zych <jzych89@gmail.com>
2019-03-11 23:50:36 +0100

    LogicalImages: Add interface layer for algorithm to solve Board

--- src/LogicalImages.cpp
+++ src/LogicalImages.cpp
@@ -1,2 +1,24 @@
 #include "LogicalImages.hpp"
+#include <memory>
+#include <iostream>
+#include "JSONFileReader.hpp"
 
+
+LogicalImages::LogicalImages(std::shared_ptr<Board> b,
+                             std::shared_ptr<Algorithm> a)
+    : board_(b),
+      algorithm_(a)
+{}
+
+LogicalImages::~LogicalImages() {}
+
+void LogicalImages::solve()
+{
+    if (algorithm_->solve(board_))
+    {
+        std::cout << "Puzzle solved!" << std::endl;
+        board_->display();
+    } else {
+        std::cout << "Solution wasn't found :(" << std::endl;
+    }
+}
